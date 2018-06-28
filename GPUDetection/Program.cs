6ce4869ce3e9e549fc72f8e77ec4e3ab40976ce7
hifon168 <bill800208@163.com>
2018-06-28 15:19:53 +0800

    新建工程

---
+++ GPUDetection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Collections;
+using System.Collections.Specialized;
+using OpenHardwareMonitor.Hardware;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+
+namespace GPUDetection
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+        }
+      
+    }
+
+}
